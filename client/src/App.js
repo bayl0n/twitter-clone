1ea19463c4be197f4e91edce695e3a9977972cb4
bayl0n <nathanbaylon@gmail.com>
2021-05-10 02:39:55 +1000

    Created basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import './App.css';
+import Home from "./pages/Home";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 
-    </div>
-  );
+            <Switch>
+                <Route path="/about">
+                    <h1>About</h1>
+                </Route>
+                <Route path="/users">
+                    <h1>Users</h1>
+                </Route>
+                <Route path="/">
+                    <h1>Home</h1>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
