622c33da8fe1c65da174c5ec687acda1d0238d09
bayl0n <nathanbaylon@gmail.com>
2021-05-19 19:34:42 +1000

    Created simple counter

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,40 @@
 <script>
-    
+    let count = 0;
+
+    let handleDecrement = () => {
+        if(count > 0) {
+            count--;
+        }
+    }
+
+    let handleIncrement = () => {
+        count++;
+    }
+
+    let handleReset = () => {
+        count = 0;
+    }
 </script>
 
 <main>
     <h1>Twitter Clone</h1>
     
     <p>Welcome to twitter clone by Nathan Baylon</p>
+
+    <h2>The count is {count} </h2>
+
+    <div>
+        <button class="btn" on:click={handleDecrement}>-</button>
+        <button class="btn" on:click={handleIncrement}>+</button>
+        <button on:click={handleReset}>Reset</button>
+    </div>
 </main>
 
 <style>
+    .btn {
+        width: 3rem;
+    }
+
 	main {
 		text-align: center;
 		padding: 1em;
